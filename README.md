ce8d74d2b5c4fedfdc91d279d0dcd49bc9d97fa5
Jason Wang <43554820+jsonW0@users.noreply.github.com>
2024-03-30 17:26:58 -0400

    simple survival analysis notebook

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Expanded Group Fairness for Survival Analysis
 Lillian Sun and Jason Wang
 
-
-Some code taken from https://github.com/umbc-sanjaylab/FISA_KDD22
+Survival analysis is the statistical problem of modeling the time until an event of interest occurs, such as death, failure, or recovery. It is widely used in various domains, including healthcare, engineering, and social sciences. However, survival analysis often faces the challenge of censorship, where the event of interest is not observed for some individuals within the study period (due to study termination or lack of follow-ups). Now, instead of observing $(x,y)$ pairs (where $y$ is output), we observe $(x,T,\delta)$ triplets, where $T$ denotes the survival time and $\delta$ is an indicator for the event occurring or the data-point being censored (i.e., the study ends and the true survival time is in the unobservable future). The presence of censorship in survival analysis data can lead to biased estimates and inaccurate predictions if not properly accounted for. For example, it may be that only the longer survival times of a particular demographic group were censored compared to another demographic group.
 
 ## Setup
 ```
@@ -11,9 +10,13 @@ conda activate fair-survival
 conda install python=3.10
 conda install -c sebp scikit-survival
 pip install jupyter
+pip install plotly
+pip install seaborn
 ```
 
 ## Code Organization
+
+We use coding conventions based off of [scikit-survival](https://scikit-survival.readthedocs.io/en/stable).
 
 `dataset_utils.py` details the loading and processing of a survival analysis dataset.
 
--- dataset_utils.py
+++ dataset_utils.py
@@ -5,7 +5,7 @@ def load_dataset(dataset_name: str) -> pd.DataFrame:
     '''
     Loads a Pandas DataFrame for the specified dataset containing the input variables and the output variable.
 
-    The `censorship` column is a boolean that is True if censored (e.g., by end of study) and False if not (e.g., event occurred).
+    The `censorship` column is a boolean that is False if censored (e.g., by end of study) and True if not (e.g., event occurred).
     The `event_time` column is a float representing the time until the event.
 
     Args:
@@ -21,7 +21,7 @@ def load_dataset(dataset_name: str) -> pd.DataFrame:
     if dataset_name=="flchain":
         dataset = pd.concat((load_flchain()[0],pd.DataFrame(load_flchain()[1])),axis=1)
         dataset.rename(columns={'death': 'censorship', 'futime': 'event_time'}, inplace=True)
-    elif dataset_name=="breast_cancer":
+    elif dataset_name=="whas500":
         dataset = pd.concat((load_whas500()[0],pd.DataFrame(load_whas500()[1])),axis=1)
         dataset.rename(columns={'fstat': 'censorship', 'lenfol': 'event_time'}, inplace=True)
     else:
@@ -29,6 +29,7 @@ def load_dataset(dataset_name: str) -> pd.DataFrame:
             dataset = pd.read_csv(dataset_name)
         except:
             raise NotImplementedError(f"Dataset not found: {dataset_name}")
+    return dataset
 
 def preprocess_dataset(dataset: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -40,3 +41,5 @@ def preprocess_dataset(dataset: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The preprocessed dataset.
     '''
+    return dataset
+    raise NotImplementedError()
