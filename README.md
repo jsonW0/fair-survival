a8b17b4de96ded35e2516643cae2fa7953a1981a
Jason Wang <43554820+jsonW0@users.noreply.github.com>
2024-03-30 16:10:30 -0400

    initial dataset work

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# fair-survival
+# Expanded Group Fairness for Survival Analysis
+Lillian Sun and Jason Wang
+
+
+Some code taken from https://github.com/umbc-sanjaylab/FISA_KDD22
+
+## Setup
+```
+conda create -n fair-survival
+conda activate fair-survival
+conda install python=3.10
+conda install -c sebp scikit-survival
+pip install jupyter
+```
+
+## Code Organization
+
+`dataset_utils.py` details the loading and processing of a survival analysis dataset.
+
+`metrics.py` details the metrics (accuracy, fairness) for evaluation on a survival analysis dataset and the survival predictions.
+
+`data/` is a folder dedicated to holding any custom datasets.
+
+`models/` is a folder dedicated to holding any saved models.
---
+++ dataset_utils.py
@@ -0,0 +1,42 @@
+from sksurv.datasets import load_flchain, load_whas500
+import pandas as pd
+
+def load_dataset(dataset_name: str) -> pd.DataFrame:
+    '''
+    Loads a Pandas DataFrame for the specified dataset containing the input variables and the output variable.
+
+    The `censorship` column is a boolean that is True if censored (e.g., by end of study) and False if not (e.g., event occurred).
+    The `event_time` column is a float representing the time until the event.
+
+    Args:
+        dataset_name (str): The name of a known dataset, or a filepath to a .csv file. If specifying a .csv file, the censorship and event_time column must exist.
+
+    Returns:
+        pd.DataFrame: The loaded dataset obeying the conventions of survival analysis (see function summary).
+
+    Raises:
+        NotImplementedError: Dataset name was unknown and the .csv file was not found.
+    '''
+    dataset = None
+    if dataset_name=="flchain":
+        dataset = pd.concat((load_flchain()[0],pd.DataFrame(load_flchain()[1])),axis=1)
+        dataset.rename(columns={'death': 'censorship', 'futime': 'event_time'}, inplace=True)
+    elif dataset_name=="breast_cancer":
+        dataset = pd.concat((load_whas500()[0],pd.DataFrame(load_whas500()[1])),axis=1)
+        dataset.rename(columns={'fstat': 'censorship', 'lenfol': 'event_time'}, inplace=True)
+    else:
+        try:
+            dataset = pd.read_csv(dataset_name)
+        except:
+            raise NotImplementedError(f"Dataset not found: {dataset_name}")
+
+def preprocess_dataset(dataset: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Preprocesses a loaded survival analysis dataset
+
+    Args:
+        dataset (pd.DataFrame): The dataset to be preprocessed.
+
+    Returns:
+        pd.DataFrame: The preprocessed dataset.
+    '''
